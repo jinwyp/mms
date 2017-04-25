3e9571839997f80a300966d3e65f36aa6eba6cdb
Daisy-dan <wudan@yimei180.com>
2017-04-25 19:26:18 +0800

    update

---
+++ frontend/wechatminiapp/src/pages/collection/collection.js
@@ -0,0 +1,27 @@
+// pages/collection/collection.js
+Page({
+  data:{
+    interest:['中文','中文','中文']
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    wx.setNavigationBarTitle({
+      title: '工时家',
+      success: function(res) {
+        // success
+      }
+    })
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
