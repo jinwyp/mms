3cd3adf1e5733ea9e58f5fd00ebe03b2adf2a598
JinWYP <jinwyp@gmail.com>
2017-04-28 17:16:27 +0800

    增加Promise 和用户登录注册UserService
    以后所有的ajax调用统一放到service目录下，具体请参考userservice.js

--- frontend/wechatminiapp/src/pages/index/index.js
+++ frontend/wechatminiapp/src/pages/index/index.js
@@ -15,35 +15,13 @@ Page({
   },
   onLoad: function () {
     console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    }),
+
     wx.setNavigationBarTitle({
       title: '工时家',
       success: function(res) {
         // success
       }
-    }),
-    wx.request({
-      url: 'https://wd.gongshijia.com', //仅为示例，并非真实的接口地址
-      data:{},
-      header: {
-          'content-type': 'application/json'
-      },
-      success: function(res) {
-        console.log(res.data.data);
-        that.setData({
-          array: res.data.data,
-          
-        })
-        
-        
-      }
     })
+    
   }
 })
