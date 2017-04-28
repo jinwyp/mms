02c8f02abe8e6b1e2057bac6b684a0afa81d0f96
Daisy-dan <wudan@yimei180.com>
2017-04-28 13:35:14 +0800

    update

--- frontend/wechatminiapp/src/pages/personInfo/personInfo.js
+++ frontend/wechatminiapp/src/pages/personInfo/personInfo.js
@@ -6,56 +6,117 @@ Page({
    nickName:'',
    userInfoAvatar:'',
    tempFilePaths:'../../images/star.png',
-   picker1Value:0,
-   picker1Range:['造型、插花','上海','广州','深圳'],
-   dateValue:new Date().getFullYear()+"-"+new Date().getMonth()+"-"+new Date().getDate()
+   allValue:'',
+   markers: [{
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: 'T.I.T 创意园',
+      desc: '我现在的位置'
+    }],
+    covers: [{
+      latitude: 23.099794,
+      longitude: 113.324520,
+      iconPath: '../../images/wechart.png',
+      rotate: 10
+    }, {
+      latitude: 23.099298,
+      longitude: 113.324129,
+      iconPath: '../../images/wechart.png',
+      rotate: 90
+    }]
   }, 
-   normalPickerBindchange:function(e){
-    this.setData({
-      picker1Value:e.detail.value
-    })
-  },
-  datePickerBindchange:function(e){
-    this.setData({
-      dateValue:e.detail.value
-    })
+   
+  getLocation:function() {
+    console.log('地图定位！')
+    var that = this
+    wx.chooseLocation({
+        type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+        success: function (res) {
+          console.log(res)
+            var latitude = res.latitude; 
+            var longitude = res.longitude; 
+            wx.openLocation({
+              latitude:latitude,
+              longitude:longitude,
+              scale:1
+            })
+        }
+    });
   },
   chooseimage: function () { 
-  var _this = this; 
-  wx.chooseImage({ 
-   count: 1, // 默认9 
-   sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有 
-   sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有 
-   success: function (res) { 
-    // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片 
-    _this.setData({ 
-     tempFilePaths:res.tempFilePaths
-    }) 
-    wx.uploadFile({
-       url: 'https://String',
-       filePath:'filePath',
-       name:'name',
-       // header: {}, // 设置请求的 header
-       // formData: {}, // HTTP 请求中其他额外的 form data
-       success: function(res){
-         // success
-       },
-       fail: function() {
-         // fail
-       },
-       complete: function() {
-         // complete
-       }
-     })
+    var _this = this; 
+    wx.chooseImage({ 
+    count: 1, // 默认9 
+    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有 
+    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有 
+    success: function (res) { 
+      // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片 
+      _this.setData({ 
+      tempFilePaths:res.tempFilePaths
+      }) 
+      wx.uploadFile({
+        url: 'https://wd.gongshijia.com/upload',
+        filePath:tempFilePaths[0],
+        name:'name',
+        // header: {}, // 设置请求的 header
+        // formData: {}, // HTTP 请求中其他额外的 form data
+        success: function(res){
+          // success
+        },
+        fail: function() {
+          // fail
+        },
+        complete: function() {
+          // complete
+        }
+      })
 
-   } 
-  }) 
+    } 
+    }) 
  } ,
+ chooseWXcode: function () { 
+    var _this = this; 
+    wx.chooseImage({ 
+    count: 1, // 默认9 
+    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有 
+    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有 
+    success: function (res) { 
+      // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片 
+      _this.setData({ 
+      userInfoAvatar:res.tempFilePaths
+      }) 
+      wx.uploadFile({
+        url: 'https://wd.gongshijia.com/upload',
+        filePath:userInfoAvatar[0],
+        name:'name',
+        // header: {}, // 设置请求的 header
+        // formData: {}, // HTTP 请求中其他额外的 form data
+        success: function(res){
+          // success
+        },
+        fail: function() {
+          // fail
+        },
+        complete: function() {
+          // complete
+        }
+      })
+
+    } 
+    }) 
+ } ,
+
  formSubmit: function(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    this.setData({
+      allValue:e.detail.value
+      })
   },
   formReset: function() {
-    console.log('form发生了reset事件')
+    console.log('form发生了reset事件');
+    this.setData({
+   allValue:''
+  })
   },
   onLoad: function () {
     var that=this;    
--- frontend/wechatminiapp/src/pages/timeSetting/timeSetting.js
+++ frontend/wechatminiapp/src/pages/timeSetting/timeSetting.js
@@ -3,9 +3,26 @@ Page({
   data:{
     date: '2016-09-01',
     time: '09:00',
-    timeDown: '18:00'
+    time2: '18:00',
+    timeDown: '18:00',
+    checked:'false'
   },
-
+  //星期选择
+  checkboxChange: function(e) {
+    // var data = {};
+    // var index = e.target.dataset.index;
+    console.log(e.target.dataset.checks)
+    if(e.target.dataset.checks === 'false'){
+      this.setData({
+        checked: 'true'
+      })
+    }else{
+      this.setData({
+        checked: 'false'
+      })
+    }
+  },
+  //时间选择
   bindTimeChange: function(e) {
     this.setData({
       time: e.detail.value
@@ -13,7 +30,20 @@ Page({
   },
   bindTimeChange2: function(e) {
     this.setData({
-      time: e.detail.value
+      time2: e.detail.value
+    })
+  },
+  //表单提交
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    this.setData({
+      allValue:e.detail.value
+    })
+  },
+  formReset: function() {
+    console.log('form发生了reset事件');
+    this.setData({
+      allValue:''
     })
   },
   onLoad:function(options){
