bde5175504f57734d85026b05e2e64c6f0eb5e4f
hary <94093146@qq.com>
2017-05-02 14:44:31 +0800

    intial adding

---
+++ backend/src/main/scala/com/gongshijia/mms/Common.scala
@@ -0,0 +1,148 @@
+package com.gongshijia.mms
+
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.StatusCodes.{BadRequest, Forbidden, InternalServerError, MethodNotAllowed}
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.Directives.{complete, extractUri}
+import akka.http.scaladsl.server._
+import akka.stream.ActorMaterializer
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsString, JsValue, JsonFormat, RootJsonFormat}
+
+import scala.util.control.NonFatal
+
+/**
+  * Created by hary on 17/1/9.
+  */
+trait Core {
+  implicit val coreSystem: ActorSystem = mkSystem
+  implicit val coreMaterializer = ActorMaterializer()
+  val coreConfig = coreSystem.settings.config
+  val log = Logging(coreSystem, this.getClass)
+
+  protected def mkSystem: ActorSystem = ActorSystem("mms-system")
+}
+
+
+object HttpResult extends DefaultJsonProtocol {
+
+  case class Result[T](data: Option[T], success: Boolean = true, error: Option[Error] = None, meta: Option[PagerInfo] = None)
+
+  case class PagerInfo(total: Int, count: Int, offset: Int, page: Int)
+
+  implicit val PagerInfoFormat = jsonFormat4(PagerInfo)
+
+  case class Error(code: Int, message: String, field: String)
+
+  implicit val ErrorFormat = jsonFormat3(Error)
+
+  implicit def resultFormat[A: JsonFormat] = jsonFormat4(Result.apply[A])
+}
+
+
+/**
+  * Created by wangqi on 16/12/16.
+  */
+case class DatabaseException(message:String) extends Exception
+case class ParameterException(message: String) extends Exception
+case class BusinessException(message: String) extends Exception
+
+
+/**
+  * Created by hary on 2017/5/2.
+  */
+
+trait CommonJsonFormat {
+
+  implicit object SqlTimestampFormat extends RootJsonFormat[Timestamp] {
+
+    override def write(obj: Timestamp) = {
+      val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+      JsString(formatter.format(obj))
+    }
+
+    override def read(json: JsValue) : Timestamp = {
+      val formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+      json match {
+        case JsString(s) => new Timestamp(formatter.parse(s).getTime)
+        case _ => throw new DeserializationException("Error info you want here ...")
+      }
+    }
+  }
+}
+
+
+trait MmsExceptionHandler extends SprayJsonSupport with Core {
+
+  import HttpResult._
+
+  implicit def myExceptionHandler: ExceptionHandler = ExceptionHandler {
+    case e: BusinessException =>
+      extractUri { uri =>
+        log.error(s"Request to $uri could not be handled normally!!!!!!!!! BusinessException")
+        complete(HttpResponse(StatusCodes.BadRequest, entity = Result(data = Some("error"), success = false, error = Some(Error(409, e.message, ""))).toJson.toString))
+      }
+
+    case e: DatabaseException =>
+      extractUri { uri =>
+        println(s"Request to $uri could not be handled normally!!!!!!!!! DatabaseException11111111 {}", e.message)
+        complete(HttpResponse(StatusCodes.BadRequest, entity = e.message))
+      }
+    case NonFatal(e) =>
+      extractUri { uri =>
+        log.error(s"Request to $uri could not be handled normally!!!!!!!!!")
+        log.error("{}", e)
+        complete(HttpResponse(StatusCodes.InternalServerError, entity = Result(data = Some("error"), success = false, error = Some(Error(500, "系统错误", ""))).toJson.toString))
+      }
+  }
+}
+
+/**
+  * Created by wangqi on 17/1/18.
+  */
+trait MmsRejectionHandler extends SprayJsonSupport with Core {
+
+  implicit def myRejectionHandler =
+    RejectionHandler.newBuilder()
+      .handle { case MissingCookieRejection(cookieName) =>
+        extractUri{ uri =>
+          log.warning(s"$uri No cookies, no service!!!")
+          complete(HttpResponse(BadRequest, entity = "No cookies, no service!!!"))
+        }
+      }
+      .handle { case MissingQueryParamRejection(name) =>
+        extractUri { uri =>
+          log.warning(s"$uri miss request parameter $name")
+          complete((Forbidden, s"miss request parameter $name"))
+        }
+      }
+      .handle { case AuthorizationFailedRejection =>
+        extractUri { uri =>
+          log.warning(s"$uri You're out of your depth!")
+          complete((Forbidden, "You're out of your depth!"))
+        }
+      }
+      .handle { case ValidationRejection(msg, _) =>
+        extractUri { uri =>
+          log.warning(s"$uri That wasn't valid! {}",msg)
+          complete((InternalServerError, "That wasn't valid! " + msg))
+        }
+      }
+      .handleAll[MethodRejection] { methodRejections =>
+      extractUri { uri =>
+        val names = methodRejections.map(_.supported.name)
+        log.warning("{} Can't do that! Supported: {} ",uri,names)
+        complete((MethodNotAllowed, s"Can't do that! Supported: ${names mkString " or "}!"))
+      }
+    }
+      .handleNotFound {
+        complete(HttpResponse(StatusCodes.NotFound, entity = "Not found"))
+      }
+      .result()
+}
+
+
---
+++ backend/src/main/scala/com/gongshijia/mms/asset/AssetRoute.scala
@@ -0,0 +1,91 @@
+package com.gongshijia.mms.asset
+
+import java.io.File
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption.{CREATE, WRITE}
+import java.util.UUID
+
+import akka.http.scaladsl.model.ContentTypes.`application/octet-stream`
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.headers.{ContentDispositionTypes, `Content-Disposition`}
+import akka.http.scaladsl.server.Directives.{complete, extractRequestContext, fileUpload, get, onSuccess, parameter, path, post}
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.FileInfo
+import akka.stream.IOResult
+import akka.stream.scaladsl.{FileIO, Keep, Source}
+import akka.util.ByteString
+import com.gongshijia.mms.Core
+import org.apache.commons.io.FileUtils
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.ContentTypes._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.{ContentDispositionTypes, `Content-Disposition`}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.FileInfo
+
+import scala.concurrent.Future
+
+/**
+  * Created by hary on 2017/5/2.
+  */
+trait AssetRoute extends Core with SprayJsonSupport {
+
+  import Models._
+
+
+  implicit val assetRouteExecutionContext = coreSystem.dispatcher
+
+  val accessId = coreSystem.settings.config.getString("aliyun.accessId")
+  val accessKey = coreSystem.settings.config.getString("aliyun.accessKey")
+
+  val fileRoot = "/tmp" // coreSystem.settings.config.getString("file.root")
+
+  //  private def getAssetInfo(id: String): Future[(String, String, Source[ByteString, Future[IOResult]])] = {
+  //    val query = assetClass.filter(f => f.asset_id === id).map { e =>
+  //      (e.url, e.file_type, e.origin_name)
+  //    }.result.head
+  //    dbrun(query).recover {
+  //      case _ =>
+  //        log.error("Database error in downloadFile")
+  //        throw new DatabaseException("该文件不存在")
+  //    } map {
+  //      case (url, fileType, name) =>
+  //        (fileType, name, FileIO.fromPath(Paths.get(fileRoot + File.separator + url)))
+  //    }
+  //  }
+
+  /**
+    * GET asset/:asset_id  -- 下载asset_id资源
+    */
+
+//  private def downloadFile: Route = get {
+//    path("download" / Segment) { id =>
+//      onSuccess(getAssetInfo(id)) {
+//        case (_, name, source) =>
+//          complete(
+//            HttpResponse(
+//              status = StatusCodes.OK,
+//              headers = List(`Content-Disposition`(ContentDispositionTypes.attachment, Map("filename" -> name))),
+//              entity = HttpEntity(`application/octet-stream`, source)
+//            )
+//          )
+//      }
+//    }
+//  }
+
+
+  // GET /asset/policy
+  def assetUploadPolicy = get {
+    path("policy") {
+      complete(PolicyResponse("callback", "signature", "policy", accessId))
+      // complete("ok")
+    }
+  }
+
+  def assetRoute = assetUploadPolicy
+
+}
+
+
