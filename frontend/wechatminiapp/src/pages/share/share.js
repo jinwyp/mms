a99298aab9c9ba4f53787c7635bc78f653071877
Daisy-dan <wudan@yimei180.com>
2017-05-03 14:31:51 +0800

    update

--- frontend/wechatminiapp/src/pages/share/share.js
+++ frontend/wechatminiapp/src/pages/share/share.js
@@ -27,6 +27,7 @@ Page({
    
    tempFilePaths:'../../images/upload.png',
    uploadImg:[],
+   uploadVideo:'',
    slideshow: false,
    allValue:'',
    markers: [{
@@ -48,9 +49,62 @@ Page({
     }]
   }, 
   
+  // 上传
+  uploadfiles:function(){
+    var that=this;
+    wx.showActionSheet({
+      itemList: ['上传照片', '上传视频'],
+      success: function(res) {  
+        // that.setData({
+        //   tapIndex: res.tapIndex
+        // })
+        // console.log(that.data.tapIndex)
+        if(res.tapIndex === 0){
+          wx.chooseImage({
+            count: 9, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+              var tempFilePaths = res.tempFilePaths;
+              that.setData({
+                uploadImg:tempFilePaths.concat(that.data.uploadImg)
+              })
+            }
+     
+          })
+        }else if(res.tapIndex === 1){
+          wx.chooseVideo({
+            sourceType: ['album', 'camera'], // album 从相册选视频，camera 使用相机拍摄
+            maxDuration: 60, // 拍摄视频最长拍摄时间，单位秒。最长支持60秒
+            camera: ['front', 'back'],
+            success: function(res){
+              // success
+              
+              that.setData({
+                  uploadVideo:res.tempFilePath
+              })
+              console.log(this.data.uploadVideo)
+            },
+            fail: function(res) {
+              // fail
+            },
+            complete: function(res) {
+              // complete
+            }
+          })
+          
+        }
+      },
+      fail: function(res) {
+        console.log(res.errMsg)
+      }
+    })
+    
+  },
   // 上传图片
-  uploadImg:function(){
-    if(this.data.uploadImg.length>=9){
+  uploadImg:function(){ 
+    var that=this;
+    if(that.data.uploadImg.length>=9){
         wx.showModal({
           title: '提示',
           content: '店内实景最多上传9张',
@@ -96,8 +150,9 @@ Page({
         }
       })
     }
-    console.log(this.data.uploadImg)
+    console.log(this.data.uploadImg)     
   },
+
   // 图片放大查看
   show: function (e) {
     var id = e.target.dataset.id,
@@ -118,21 +173,40 @@ Page({
   delImg:function(e){
     var that=this;
      wx.showModal({
-          title: '提示',
-          content: '您确认删除该张照片吗',
-          showCancel:true,
-          success: function(res) {
-            if (res.confirm) {
-              var index=e.target.dataset.id;
-              that.data.uploadImg.splice(index,1)
-              that.setData({
-                  uploadImg:that.data.uploadImg
-              });
-            } else if (res.cancel) {
-              console.log('用户点击取消')
-            }
+        title: '提示',
+        content: '您确认删除该张照片吗',
+        showCancel:true,
+        success: function(res) {
+          if (res.confirm) {
+            var index=e.target.dataset.id;
+            that.data.uploadImg.splice(index,1)
+            that.setData({
+                uploadImg:that.data.uploadImg
+            });
+          } else if (res.cancel) {
+            console.log('用户点击取消')
           }
-        })
+        }
+      })
+  },
+  delVideo:function(){
+    var that=this;
+     wx.showModal({
+        title: '提示',
+        content: '您确认删除该视频吗',
+        showCancel:true,
+        success: function(res) {
+          if (res.confirm) {
+            // var index=e.target.dataset.id;
+            // that.data.uploadVideo.splice(index,1)
+            that.setData({
+                uploadVideo:''
+            });
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
   },
   // 选择时间
   datePickerBindchange:function(e){
@@ -163,37 +237,7 @@ Page({
         }
     });
   },
-//   chooseimage: function () { 
-//     var _this = this; 
-//     wx.chooseImage({ 
-//     count: 1, // 默认9 
-//     sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有 
-//     sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有 
-//     success: function (res) { 
-//       // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片 
-//       _this.setData({ 
-//       tempFilePaths:res.tempFilePaths
-//       }) 
-//       wx.uploadFile({
-//         url: 'https://wd.gongshijia.com/upload',
-//         filePath:tempFilePaths[0],
-//         name:'name',
-//         // header: {}, // 设置请求的 header
-//         // formData: {}, // HTTP 请求中其他额外的 form data
-//         success: function(res){
-//           // success
-//         },
-//         fail: function() {
-//           // fail
-//         },
-//         complete: function() {
-//           // complete
-//         }
-//       })
 
-//     } 
-//     }) 
-//  } ,
  radioChange: function(e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
     this.setData({
