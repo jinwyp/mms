5e0031ba773f307b4a4f3a6503af111ef2dfc2cd
xiangyang <xiangyang2516@gmail.com>
2017-05-10 08:33:32 +0800

    增加mongoDB演示

--- backend/src/main/scala/com/gongshijia/mms/test/MongoRoute.scala
+++ backend/src/main/scala/com/gongshijia/mms/test/MongoRoute.scala
@@ -1,42 +1,57 @@
 package com.gongshijia.mms.test
 
+
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.Directives.{complete, get, path, post, _}
+import akka.http.scaladsl.server.Directives._
 import com.gongshijia.mms.Core
-import com.gongshijia.mms.test.Models.Person
 import org.mongodb.scala._
-import org.mongodb.scala.model.Filters.equal
-import org.mongodb.scala.model.Updates.set
+import org.mongodb.scala.bson.ObjectId
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Updates._
+
+// Create a codec for the Person case class
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
 import org.mongodb.scala.bson.codecs.Macros._
+
 /**
   * Created by hary on 2017/5/3.
   */
-trait MongoRoute extends Core with SprayJsonSupport {
+trait MongoRoute extends Models with Core with SprayJsonSupport {
 
 
-  val codecRegistry = fromRegistries(fromProviders(classOf[Person]), DEFAULT_CODEC_REGISTRY)
+  //
+  val codecRegistry = fromRegistries(fromProviders(classOf[PersonDTO], classOf[Address], classOf[Course]), DEFAULT_CODEC_REGISTRY);
+
 
   // mongo example
   def mongoInsert = path("insert") {
+    val mongoClient: MongoClient = MongoClient();
+    val database: MongoDatabase = mongoClient.getDatabase("test").withCodecRegistry(codecRegistry)
+    val collection: MongoCollection[PersonDTO] = database.getCollection("userinfo")
     post {
+      entity(as[Person]) { person =>
+        println("----------------------------------------");
+        println(person.toJson);
+        println("----------------------------------------");
+        val dto = PersonDTO(new ObjectId(),person.firstName, person.lastName, person.registerDate, person.fullName, person.address, person.courses);
+        onSuccess(collection.insertOne(dto).toFuture().map(_ => true)) {
+          case true => complete(person);
+        }
+      }
+    }
+  }
+
+  def mongoSelect = path("select") {
+    get {
       val mongoClient: MongoClient = MongoClient();
       val database: MongoDatabase = mongoClient.getDatabase("test").withCodecRegistry(codecRegistry)
-      val collection: MongoCollection[Person] = database.getCollection("userinfo")
-      collection.drop().results();
-
-      val person: Person = Person("A", "A")
-      collection.insertOne(person).results()
-
-
-      val people: Seq[Person] = Seq(
-        Person("B", "B"),
-        Person("C", "C")
-      )
-      collection.insertMany(people).printResults()
-      complete("ok")
-
+      val collection: MongoCollection[PersonDTO] = database.getCollection("userinfo")
+      onSuccess(collection.find().toFuture()) {
+        persons => {
+          complete(persons)
+        };
+      }
     }
   }
 
@@ -62,18 +77,12 @@ trait MongoRoute extends Core with SprayJsonSupport {
       val mongoClient: MongoClient = MongoClient();
       val database: MongoDatabase = mongoClient.getDatabase("test").withCodecRegistry(codecRegistry)
       val collection: MongoCollection[Person] = database.getCollection("userinfo")
-
-//      collection.find().limit(10).skip();
-      mongoClient.close();
-      complete("ok")
+      onSuccess(collection.find().limit(5).skip(2).toFuture()) {
+        persons => complete(persons);
+      }
     }
   }
 
-  def mongoSelect = path("select") {
-    get {
-      complete("ok")
-    }
-  }
 
   def mongoRoute = mongoInsert ~ mongoUpsert ~ mongoDelete ~ mongoPager ~ mongoSelect
 }
