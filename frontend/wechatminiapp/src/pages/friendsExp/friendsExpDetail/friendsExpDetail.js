646b734f6c777586ed6a2764d3fa9d9ba55548a6
chenluily0451 <chenluily@163.com>
2017-05-20 10:21:37 +0800

    commit

--- frontend/wechatminiapp/src/pages/friendsExp/friendsExpDetail/friendsExpDetail.js
+++ frontend/wechatminiapp/src/pages/friendsExp/friendsExpDetail/friendsExpDetail.js
@@ -13,10 +13,9 @@ Page({
     videos:'',
     feeling:'',
     signInfo:[],
+    shopName:'',
     currentId:0,
     collectText:"+ 收藏",
-    shopName:"工时期王靓店",
-    shopAddress:"上海市彭浦新村",
     hasCollect:false,
     slideorder:true,
     hrefId:'',
@@ -41,16 +40,16 @@ Page({
       costTime:'40',
       gyprocess:'流程十分复杂流程十分复杂流程十分复杂流程十分复杂流程十分复杂流程十分复杂',
       comments:[
-          {
-            commentsName:"COCO",
-            time:'2013-1-21 18:00',
-            message:'真的很漂亮~~~'
-          },
-          {
-            commentsName:"Lu",
-            time:'2014-2-4 18:00',
-            message:'时代变了，曾经打一局电子竞技游戏需要端坐在电脑前，一局游戏没有半个小时不会完结。随着 4G 网络和智能手机的普及，以及手游的快节奏，现在随时随地都可以呼朋唤友来一局，尤其是在吃海底捞排队，或者飞机延误这种等待时间里，手游，尤其是竞技性更强手游，成为了 Kill Time 的更佳选择。'
-          }
+          // {
+          //   commentsName:"COCO",
+          //   time:'2013-1-21 18:00',
+          //   message:'真的很漂亮~~~'
+          // },
+          // {
+          //   commentsName:"Lu",
+          //   time:'2014-2-4 18:00',
+          //   message:'时代变了，曾经打一局电子竞技游戏需要端坐在电脑前，一局游戏没有半个小时不会完结。随着 4G 网络和智能手机的普及，以及手游的快节奏，现在随时随地都可以呼朋唤友来一局，尤其是在吃海底捞排队，或者飞机延误这种等待时间里，手游，尤其是竞技性更强手游，成为了 Kill Time 的更佳选择。'
+          // }
         ],
     expLists:{
         ifvideo:false,
@@ -67,11 +66,14 @@ Page({
   },
   onLoad:function(options){
 
-    var id = options.id;
+    var id = options.id,pid = options.pid;
     var that = this;
     var openId = wx.getStorageSync('accessToken')
+
+    console.log(options.id + '@@')
+    console.log(options.pid + '@@')
   
-    if (openId) {
+    if (openId && id) {
         CategoryService.friendDetail('',id).then(function(res){
           console.log(res.data.avatarUrl)
           that.setData({
@@ -82,9 +84,39 @@ Page({
             pictures: res.data.pictures,
             feeling: res.data.feeling,
             signInfo: res.data.signInfo,
-            hrefId:res.data._id
+            hrefId:res.data._id,
+            comments: res.data.comments,
+            checked:res.data.checked,
+            shopName: res.data.shopName,
+            locationName: res.data.locationName,
+            signId:res.data.signInfo[0]._id
           })
         }).catch(Error.PromiseError)
+    }
+
+    if (openId && pid) {
+      var mkdata={
+        reportId: id,
+        signInfoId: pid
+      }
+      CategoryService.makeSureReport(mkdata).then(function (res) {
+        console.log(res.data.avatarUrl)
+        that.setData({
+          videos: res.data.videos,
+          avatarUrl: res.data.avatarUrl,
+          nickName: res.data.nickName,
+          expTime: res.data.expTime,
+          pictures: res.data.pictures,
+          feeling: res.data.feeling,
+          signInfo: res.data.signInfo,
+          hrefId: res.data._id,
+          comments: res.data.comments,
+          checked: res.data.checked,
+          shopName: res.data.shopName,
+          locationName: res.data.locationName,
+          signId: res.data.signInfo[0]._id
+        })
+      }).catch(Error.PromiseError)
     }
   },
   oncollect:function(){
@@ -93,22 +125,17 @@ Page({
           collectText:'已收藏',
           hasCollect:true
         })
-        wx.showToast({
-          title: '已收藏',
-          icon: 'success',
-          duration: 2000,
-          mask:true
-        })
+  
+      var data={
+        reportId: this.data.hrefId
+      }
+        CategoryService.addReportToCollect(data).then(function (res) {
+          console.log(res);
+        }).catch(Error.PromiseError)
     }else{
       this.setData({
           collectText:'+ 收藏',
           hasCollect:false
-        })
-        wx.showToast({
-          title: '已取消',
-          icon: 'loading',
-          duration: 2000,
-          mask:true
         })
     }
   },
@@ -129,32 +156,53 @@ Page({
   submitMessage:function(){
     var that=this;
     var commentInfo= [{
-          commentsName:"匿名",
-          time:"xxxx-xx-xx",
-          message:this.data.inputVal
-        }]
-        if(this.data.inputVal.length){
-          var newArray=this.data.comments.concat(commentInfo);
-          this.setData({
-            comments:newArray
-          })
-          wx.showToast({
-            title: '已提交',
-            icon: 'success',
-            duration: 2000,
-            mask:true
-            })
-          }else{
-            wx.showToast({
-              title: '内容不能为空',
-              icon: 'loading',
-              duration: 2000,
-              mask:true
-            })
-          }
-        this.setData({
-            inputVal:''
+      commentsName:"匿名",
+      time:"xxxx-xx-xx",
+      content:this.data.inputVal
+    }]
+    var data={
+      "reportid": this.data.hrefId,
+      "openid": wx.getStorageSync('accessToken'),
+      "content": this.data.inputVal
+    }
+    if(this.data.inputVal.length){
+      var newArray = this.data.comments.concat(commentInfo);
+      CategoryService.addComment(data).then(function (res) {
+        wx.showToast({
+          title: '提交中',
+          icon: 'loading',
+          duration: 5000,
+          mask: true
         })
+        if(res.success){
+          wx.hideToast();
+          that.setData({
+            comments: newArray
+          })
+        }else{
+          wx.showModal({
+            title: '提示',
+            content: '评论失败',
+            showCancel: false,
+            success: function (res) {
+
+            }
+          })
+        }
+      }).catch(Error.PromiseError)
+    }else{
+      wx.showModal({
+        title: '提示',
+        content: '评论内容不能空',
+        showCancel:false,
+        success: function (res) {
+
+        }
+      })
+    }
+    this.setData({
+        inputVal:''
+    })
   },
   inputFun:function(e){
     var that=this;
@@ -164,10 +212,34 @@ Page({
     })
   },
   tapSwitch : function(){
-    console.log(this.data.isClick)
     this.setData({
         isClick: !this.data.isClick
     });
+    var isClick = this.data.isClick,that = this
+    if (isClick) {
+      wx.showModal({
+        title: '提示',
+        content: '您确认对他进行署名确认？',
+        success: function (res) {
+          if (res.confirm) {
+
+              var data = {
+                reportId: that.data.hrefId,
+                signInfoId: that.data.signId
+              }
+              CategoryService.handlerMakeSureReport(data).then(function (res) {
+                console.log(res)
+              }).catch(Error.PromiseError)
+
+          } else if (res.cancel) {
+            that.setData({
+              isClick: false
+            });
+          }
+        }
+      })
+    }
+   
   },
   addProcess : function(){
     var that = this 
