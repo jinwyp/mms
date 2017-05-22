ee10958b9cda04148d624d818f37397933c1380b
xiangyang <xiangyang2516@gmail.com>
2017-05-22 18:53:30 +0800

    修改测试后端地址

--- frontend/wechatminiapp/src/service/apiPath.js
+++ frontend/wechatminiapp/src/service/apiPath.js
@@ -1,6 +1,6 @@
 
-var apiPrefix = 'http://zxy.gongshijia.com'
-// var apiPrefix = 'http://gongshijia.com.ngrok.io'
+//var apiPrefix = 'http://zxy.gongshijia.com'
+ var apiPrefix = 'http://gongshijia.com.ngrok.io'
 
 var apiPath = {
     ossUrl: 'https://gsjtest.oss-cn-shanghai.aliyuncs.com/',
